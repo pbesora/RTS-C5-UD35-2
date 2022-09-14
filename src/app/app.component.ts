9c19ab9155f0e04a92d310a22c60927228d344eb
pbesora <pau_besora@hotmail.com>
2022-09-14 23:24:06 +0200

    Project updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'UD35-2';
+
+  emailRejected:String = "Introduzca un email valido";
+  valRejected:String = "Validación anti-spam incorrecta";
+  valMsg:any;
+  emailMsg:any;
+  datos:any;
+
+  uploadData(nombre:any, email:any, mensaje:any, suma:any){
+    const expression: RegExp = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+    if (!expression.test(email)) {
+      this.emailMsg = this.emailRejected;
+    }else if(suma != 5){
+      this.valMsg = this.valRejected;
+    }else{
+      this.datos={nombre:nombre, email:email, mensaje:mensaje};
+    }
+
+  }
+
 }
